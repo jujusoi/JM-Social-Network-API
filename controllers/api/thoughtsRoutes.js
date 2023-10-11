54ec37d555a0a45c72a925594c2657eef39ce793
jujusoi <jalxmcdonald@hotmail.com>
2023-10-11 19:20:32 +1030

    added thought individual get and post

--- controllers/api/thoughtsRoutes.js
+++ controllers/api/thoughtsRoutes.js
@@ -1,15 +1,33 @@
 const thoughts = require('express').Router();
 const { Thought } = require('../../models');
 
-thoughts.get('/', async (req, res) => {
+thoughts
+.get('/', async (req, res) => {
     try {
-        const thoughtData = await Thought.find().select('-__v');
+        const thoughtData = await Thought.find().populate('reactions', '-__v').select('-__v');
         res.status(200).json(thoughtData);
     } catch (err) {
         res.status(500).json(err);
     }
-}).post('/', (req, res) => {
-    res.status(200).json(`Post works`);
+}).get('/:thoughtId', async (req, res) => {
+    try {
+        const thoughtData = await Thought.findOne({ _id: req.params.thoughtId }).populate('reactions', '-__v').select('-__v');
+        if (thoughtData) {
+            res.status(200).json(thoughtData);
+        } else {
+            res.status(404).json(`ThoughtId not found`);
+        };
+    } catch (err) {
+        res.status(500).json(err);
+    };
+})
+.post('/', async (req, res) => {
+    try {
+        const newThought = await Thought.create(req.body);
+        res.status(200).json(`Thought created`);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = thoughts;
